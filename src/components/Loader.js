3b38ef6d6edb230ae9f537fa7e303a11fb7f4b86
Shaleen Agarwal <shaleen76@gmail.com>
2018-07-17 00:44:33 -0500

    Add validations

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,7 +4,7 @@ class Loader extends Component  {
     render()    {
         return(
             <div className = "ui active inverted dimmer">
-              <div className = "ui large text loader">Loading Secret Santa Data...</div>
+              <div className = "ui large text loader">Cannot start Secret Santa, due to lack of User Data. Please try again ... </div>
             </div>
         );
     }
---
+++ src/components/Validation.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+require('../../style.less');
+
+class Validation extends Component  {
+    render()    {
+        return(
+            <div className = "ui warning message">
+                <div className = "header">
+                    Insufficient data
+                </div>
+                <ul className = "list">
+                    <li>
+                        Insufficient data in the database. Please check the users.json file.
+                    </li>
+                    <li>
+                        Ensure that the number of users are more than 1 to play the Secret Santa game.
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Validation;
